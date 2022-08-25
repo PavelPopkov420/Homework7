796b27ad148abe4d91cb38461e89fe4e3566d7db
Pavel Popkov <goil2011@yandex.ru>
2022-08-25 18:13:58 +0300

    task 47

---
+++ taks47/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите количество строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+double [,] matrix = new double [n, m];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    matrix[i, j] = new Random().Next(-5,5) + new Random().NextDouble();
+      
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    matrix[i, j] = Math.Round(matrix[i,j], 3);
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+        Console.Write(matrix[i,j] + " \t");
+    Console.WriteLine();
+}
