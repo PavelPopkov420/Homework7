1680c4e463dfdf0c72725dd727171748d5f3a9d2
Pavel Popkov <goil2011@yandex.ru>
2022-08-25 18:22:06 +0300

    task53

---
+++ task53/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число строк: ");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,]matrix =new int [n, m];
+
+int sum = 0;
+float middle = 0;
+        for (int i=0; i < n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                    matrix[i, j] = new Random().Next(0, 5);
+                    sum += matrix[i, j];
+                    middle = sum /( m*n);
+
+                Console.Write(matrix [i, j] + "\t");
+
+
+            }
+            Console.WriteLine();
+
+        }
+        Console.WriteLine("среднее арефметическое столбцов: " + middle);
